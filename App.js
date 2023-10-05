949315e38f0e0187e67d3b40c015e2b6048e57db
madhusudan <madhusudanghulev@gmail.com>
2023-10-06 01:24:22 +0530

    netiv init

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    
+      <Text>Open up App.js to start!</Text>
+     
+  )
+   
+}
+
